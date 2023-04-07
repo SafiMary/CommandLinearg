b9cd60ba5fa6905a6ed443f52487153941e00b27
С - 7 < - 7@WIN-1B59ROQRL87>
2023-04-07 21:21:49 +0500

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLinearg
+{
+    internal class Person
+    {
+        private string name;
+        /* private int age;*/
+        public string Name { /*set;*/ get { return name; } }
+        public int Age { set; get; }
+
+       /* public int MyProperty
+        {
+            get { return myVar; }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    myVar = value;
+                }
+                else
+                {
+                    myVar = 0;
+                }
+            }
+        }*/
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {Name}\nвозраст: {Age}");//вывод имя и возраста в одном выводе
+        } //Console.WriteLine("Имя{0} возраст{1}",Name,Age);
+        public Person(string _name)
+        {
+            name = _name;
+        }                                           
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLinearg
+{
+    struct MyStruct
+    {
+
+    }
+
+    internal class Program
+    {
+        static List<Person> Ask_N_Times(int n)
+        {
+            //List<Person> ans = new List<Person>();
+            var ans = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                ans.Add(CreatePerson());
+            }
+            return ans;
+        }
+        static Person CreatePerson()
+        {
+            string name;
+            int age = 0;
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            do {
+                try
+                {
+                    Console.WriteLine("Введите возраст");
+                    age = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+
+                    Console.WriteLine("Возраст введен не корректно!");
+                }
+            } while ((age <= 0) && (age > 120));
+            var person = new Person(name);
+            person.Age = age;
+            return person;
+        } 
+        static void Main(string[] args)
+        {
+           // Person person = new Person("Bilbo Baggins");
+            //person.MyProperty = 1;
+            //Console.WriteLine(person.MyProperty);
+           // person.Age = 5;
+            //person.Print();
+            
+            /* string name;
+             int age;
+             Console.WriteLine("Введите имя");
+             name = Console.ReadLine();
+             Console.WriteLine("Введите возраст");
+             age = int.Parse(Console.ReadLine());
+             var person = new Person(name);
+             person.Age = age;*/
+           /* var _list = new List<Person>();
+            var person = CreatePerson();
+            _list.Add(person);
+            _list[0].Print();*/
+            var PersonList = Ask_N_Times(3);
+            foreach (Person person in PersonList)
+            {
+                Console.WriteLine($"Имя {person.Name},$ возраст {person.Age}");
+            }
+
+        }
+    }
+}
